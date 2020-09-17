eb16946bed0beeed9b3147625d74fb6f4a10a293
erika kettleson <erikakettleson@gmail.com>
2020-09-17 11:42:11 -0700

    calls working, mostly hardcoded

--- app/forms.py
+++ app/forms.py
@@ -1,18 +1,21 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectMultipleField, widgets
 from wtforms.validators import DataRequired
 
-
-class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Sign In')
+class MultiCheckboxField(SelectMultipleField):
+    widget = widgets.ListWidget(prefix_label=False)
+    option_widget = widgets.CheckboxInput()
 
 
 class AddressForm(FlaskForm):
+    sock_options = ['Mohair\rFur\rMesh\rCrochet\rTufted\n']
+    list_of_socks = sock_options[0].split()
+    # create a list of value/description tuples
+    options = [(x, x) for x in list_of_socks]
+
+    socks = MultiCheckboxField('Socks', choices=options)
     name = StringField('Name', validators=[DataRequired()])
-    phone = StringField('Form', validators=[DataRequired()])
+    phone = StringField('Phone', validators=[DataRequired()])
     street = StringField('Street', validators=[DataRequired()])
     city = StringField('City', validators=[DataRequired()])
     state = StringField('State', validators=[DataRequired()])
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
 from flask import render_template, flash, redirect, url_for
 from app import app
-from app.forms import LoginForm, AddressForm
+from app.forms import AddressForm
 import requests
+import json
 
 
 @app.route('/')
@@ -20,70 +21,67 @@ def index():
             'name': 'Mesh',
             'size': 'One-Size'
         },
+        {
+            'name': 'Tufted',
+            'size': 'One-Size'
+        },
+        {
+            'name': 'Crochet',
+            'size': 'One-Size'
+        },
     ]
     return render_template('index.html', title='Home', socks=socks)
 
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect(url_for('index'))
-    return render_template('login.html',  title='Sign In', form=form)
 
 @app.route('/order', methods=['GET', 'POST'])
 def order():
     form = AddressForm()
     if form.validate_on_submit():
-        dropoff_address = form.street.data + ' ' + form.city.data + ' ' + form.state.data
+        dropoff_address = form.street.data + ' ' + \
+            form.city.data + ' ' + form.state.data
         print('Quote Submitted for address {}'.format(dropoff_address))
 
         quote_data = {
-            'dropoff_address': dropoff_address, 
-            'pickup_address': '153 Lexington St. San Francisco, CA'
-            }
+            'dropoff_address': dropoff_address,
+            'pickup_address': '155 Lexington St. San Francisco, CA'
+        }
         auth = {
-            'Content-Type': 'application/x-www-form-urlencoded', 
+            'Content-Type': 'application/x-www-form-urlencoded',
             'Authorization': 'Basic NGJkNTFmMWQtM2ViMS00ZTM0LTkyOWUtOGMxMGU1OTI2ZmNhOg=='
-            }
-        res = requests.post('https://api.postmates.com/v1/customers/cus_MpuW-m0uomjj7F/delivery_quotes', data=quote_data, headers=auth)
-        
+        }
+        res = requests.post(
+            'https://api.postmates.com/v1/customers/cus_MpuW-m0uomjj7F/delivery_quotes', data=quote_data, headers=auth)
         quote_id = res.json()['id']
-                
+
         if res.status_code != 200:
             return _('We\'re Sorry: Delivery not available.')
         else:
-            print('Submitted Quote, got id {}, about to Submit Order {}'.format(quote_id, dropoff_address))
+            print('Submitted Quote, got id {}, about to Submit Order {}'.format(
+                quote_id, dropoff_address))
 
+            items = [
+                {
+                'name': 'Mohair',
+                'quantity': 1,
+                'size': 'small'
+                }
+            ]
 
-            items = [{
-                "name": "Mohair",
-                "quantity": "1",
-                "size": "large"
-            },{
-                "name": "Mesh",
-                "quantity": "1",
-                "size": "large"
-            }]
             order_data = {
                 'dropoff_address': dropoff_address,
-                'pickup_address': '153 Lexington St. San Francisco, CA',
+                'dropoff_name': 'erika',
+                'dropoff_phone_number': '9085918760',
+                'pickup_address': '155 Lexington St. San Francisco, CA',
                 'pickup_name': 'SF Socks Store',
-                'pickup_phone_number': '8888157726',
-                'dropoff_name': form.name.data,
-                'dropoff_phone_number': form.phone.data,
+                'pickup_phone_number': '9082477262',
+                'quote_id': quote_id,
                 'manifest': 'socks',
-                'manifest_items': items,
-                'quote_id': quote_id
-                }
-            
-            
-            resp = requests.post('https://api.postmates.com/v1/customers/cus_MpuW-m0uomjj7F/deliveries', data=order_data, headers=auth)
-            
-            import ipdb; ipdb.set_trace() 
+                'manifest_items': json.dumps(items),
+            }
 
+            resp = requests.post(
+                'https://api.postmates.com/v1/customers/cus_MpuW-m0uomjj7F/deliveries', data=order_data, headers=auth)
 
-        
+            print(resp.status_code, resp.json())
+
     return render_template('order.html', title='Order Socks', form=form)
